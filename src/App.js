5d2b26f700b965a9c6b0dda603945cd0420e2e04
adebowale <ademibowaleadegboye@gmail.com>
2022-09-09 09:42:58 +0100

    .

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+import Header from './components/Header';
+import Section from './components/Section';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Home/>
+     <Section/> 
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Section() {
+  return (
+    <wrap >
+      <div className="section-b" >
+       <div class="model-s">
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+        </div>
+        <div> 
+        <div className="button-page">
+        <button className="button-one">Customer Order</button>
+        <button className="button-two">Existing Inventory</button>
+        
+        </div>
+        <div class="scroll-down"></div>
+        </div> 
+        </div>
+    </wrap>
+  )
+}
+
+export default Section
+
